f9e5582e47fee59c33ade45c4d79448d7d34408c
Mohit chouhan <152206780+MohitChouhan04@users.noreply.github.com>
2026-02-19 22:24:45 +0530

    integrate db

--- src/app/loginpage/loginpage.spec.ts
+++ src/app/loginpage/loginpage.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Loginpage } from './loginpage';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('Loginpage', () => {
   let component: Loginpage;
@@ -8,13 +10,18 @@ describe('Loginpage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Loginpage]
+      imports: [
+        Loginpage,
+        FormsModule,          // ✅ ADD THIS
+        RouterTestingModule   // ✅ ADD THIS
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(Loginpage);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    // await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
